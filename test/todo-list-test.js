5737cc64c10e78db07ac3b4ffa4da5bce5419367
Matteo Vaccari <vaccari@pobox.com>
2016-07-03 19:14:06 +0200

    cleanup

--- test/todo-list-test.js
+++ test/todo-list-test.js
@@ -10,26 +10,21 @@ describe('the todolist view', function() {
     $all = function(selector) { return fixture.querySelectorAll(selector); }
   })
 
-  describe('an empty todo list', function() {
-    it('returns an empty html list', function() {
-      expect(new TodoListView([]).render()).to.equal('<ul class="todo-list"></ul>');
-    });
+  it('renders an empty todo list', function() {
+    expect(new TodoListView([]).render()).to.equal('<ul class="todo-list"></ul>');
   });
 
-  describe('a list of one element', function() {
-    it('renders as html', function() {
-      fixture.innerHTML = new TodoListView(['Pippo']).render();
-      expect($('ul.todo-list li label').textContent).equal('Pippo');
-      expect($('ul.todo-list input.edit').value).equal('Pippo');
-    });
+  it('renders a list of one element', function() {
+    fixture.innerHTML = new TodoListView(['Pippo']).render();
+    expect($('ul.todo-list li label').textContent).equal('Pippo');
+    expect($('ul.todo-list input.edit').value).equal('Pippo');
   });
 
-  describe('a list of two elements', function() {
-    it('renders as html', function() {
-      fixture.innerHTML = new TodoListView(['Foo', 'Bar']).render();
-      expect($all('ul.todo-list li label')[0].textContent).equal('Foo');
-      expect($all('ul.todo-list li label')[1].textContent).equal('Bar');
-    });
+  it('renders a list of two elements', function() {
+    fixture.innerHTML = new TodoListView(['Foo', 'Bar']).render();
+    var actualLabels = $all('ul.todo-list li label');
+    expect(actualLabels[0].textContent).equal('Foo');
+    expect(actualLabels[1].textContent).equal('Bar');
   });
 });
 
