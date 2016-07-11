374e2f86807e81d681aac129e125c474819ee49d
Matteo Vaccari <vaccari@pobox.com>
2016-07-11 14:01:35 +0200

    refactor

--- test/todo-list-test.js
+++ test/todo-list-test.js
@@ -1,43 +1,5 @@
 'use strict';
 
-describe('the todoItem model', function() {
-  var todoItem;
-
-  beforeEach(function() {
-    todoItem = new TodoItem('aaa');
-  })
-
-  it('returns its text', function() {
-    expect(todoItem.text()).equal('aaa')
-  });
-
-  it('can be completed and uncompleted', function() {
-    expect(todoItem.isCompleted()).equal(false)
-
-    todoItem.complete(true);
-
-    expect(todoItem.isCompleted()).equal(true)
-
-    todoItem.complete(false);
-
-    expect(todoItem.isCompleted()).equal(false)
-  });
-
-  it('can be renamed', function() {
-    todoItem.rename('bbb');
-
-    expect(todoItem.text()).equal('bbb')
-  });
-
-  it('trims before renaming', function() {
-    todoItem.rename('  x  ');
-
-    expect(todoItem.text()).equal('x')
-  });
-
-});
-
-
 describe('the todolist model', function() {
   var todoList;
 
