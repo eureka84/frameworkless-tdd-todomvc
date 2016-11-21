d8d101b79bb849e4c4299c60b6f8f3589d997e7e
Matteo Vaccari <vaccari@pobox.com>
2016-11-21 17:32:44 +0100

    use ES6 templates

--- js/todo_item_view.js
+++ js/todo_item_view.js
@@ -1,26 +1,24 @@
 function todoItemHtml(todoItem, index) {
-  var template =
-      '<div class="view">' +
-        '<input class="toggle" type="checkbox" {{checked}}>' +
-        '<label>{{text}}</label>' +
-        '<button class="destroy"></button>' +
-      '</div>' +
-      '<input class="edit" value="{{text}}">';
-  return template.
-    replace(/{{text}}/g, escapeEntities(todoItem.text())).
-    replace(/{{checked}}/, todoItem.isCompleted() ? 'checked="checked"' : '');
+  var text = escapeEntities(todoItem.text());
+  var checked = todoItem.isCompleted() ? 'checked="checked"' : '';
+  return `<div class="view">
+            <input class="toggle" type="checkbox" ${checked}>
+            <label>${text}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${text}">`;
 }
 
-function TodoItemView(todoItem, element, index) {  
+function TodoItemView(todoItem, element, index) {
   var self = this;
-  
+
   this.render = function() {
     element.innerHTML = todoItemHtml(todoItem, index);
     if (todoItem.isCompleted()) {
       element.setAttribute('class', 'completed')
     }
     element.setAttribute('data-index', index)
-    
+
     element.querySelector('input').onchange = function(event) {
       todoItem.complete(event.target.checked);
       self.render();
