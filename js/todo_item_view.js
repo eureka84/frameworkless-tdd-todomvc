08cffda530107675e9bd43ab9f8a8cb95afa2ed5
Matteo Vaccari <vaccari@pobox.com>
2016-11-26 17:22:30 +0100

    moved editing handlers to todo item view

--- js/todo_item_view.js
+++ js/todo_item_view.js
@@ -12,6 +12,59 @@ function todoItemHtml(todoItem, index) {
 function TodoItemView(todoItem, element, index) {
   var self = this;
 
+  function attachHandlerForEditing(listItem) {
+    var ESC_KEY = 27, RETURN_KEY = 13;
+
+    var view = listItem.querySelector('.view');
+    var editField = listItem.querySelector('input.edit');
+
+    function stopEditing() {
+      listItem.className = '';
+      view.style.display = 'block';
+      editField.style.display = 'none';
+    }
+
+    function startEditing() {
+      listItem.className = 'editing';
+      view.style.display = 'none';
+      editField.style.display = 'block';
+      editField.selectionStart = editField.selectionEnd = todoItem.text().length;
+      editField.focus();
+    }
+
+    function rename() {
+      if (editField.value.trim() == '')
+        todoItem.destroy();
+      else
+        todoItem.rename(editField.value);
+    }
+
+    function cancelEdit() {
+      editField.value = todoItem.text();
+    }
+
+    listItem.onkeyup = function(event) {
+      if (event.keyCode == ESC_KEY) {
+        editField.onblur = undefined;
+        cancelEdit();
+        stopEditing();
+      }
+      else if (event.keyCode == RETURN_KEY) {
+        editField.onblur = undefined;
+        rename();
+        stopEditing();
+      }
+    }
+
+    listItem.ondblclick = function(event) {
+      startEditing();
+      editField.onblur = function(ev) {
+        stopEditing();
+        rename();
+      }
+    };
+  }
+
   this.render = function() {
     element.innerHTML = todoItemHtml(todoItem, index);
     if (todoItem.isCompleted()) {
@@ -27,5 +80,7 @@ function TodoItemView(todoItem, element, index) {
     element.querySelector('button.destroy').onclick = function(event) {
       todoItem.destroy();
     };
+
+    attachHandlerForEditing(element);
   }
 }
--- js/todo_list_view.js
+++ js/todo_list_view.js
@@ -2,62 +2,6 @@ function TodoListView(todoList, document) {
   var self = this;
   todoList.addObserver(self);
 
-  function attachListenersForEditing() {
-    var ESC_KEY = 27, RETURN_KEY = 13;
-
-    document.querySelectorAll('ul.todo-list li').forEach(function(listItem) {
-      var index = listItem.attributes['data-index'].value;
-      var view = listItem.querySelector('.view');
-      var editField = listItem.querySelector('input.edit');
-
-      function stopEditing() {
-        listItem.className = '';
-        view.style.display = 'block';
-        editField.style.display = 'none';
-      }
-
-      function startEditing() {
-        listItem.className = 'editing';
-        view.style.display = 'none';
-        editField.style.display = 'block';
-        editField.selectionStart = editField.selectionEnd = todoList.at(index).text().length;
-        editField.focus();
-      }
-
-      function rename() {
-        if (editField.value.trim() == '')
-          todoList.destroy(todoList.at(index));
-        else
-          todoList.at(index).rename(editField.value);
-      }
-
-      function cancelEdit() {
-        editField.value = todoList.at(index).text();
-      }
-
-      listItem.onkeyup = function(event) {
-        if (event.keyCode == ESC_KEY) {
-          editField.onblur = undefined;
-          cancelEdit();
-          stopEditing();
-        }
-        else if (event.keyCode == RETURN_KEY) {
-          editField.onblur = undefined;
-          rename();
-          stopEditing();
-        }
-      }
-
-	    listItem.ondblclick = function(event) {
-	      startEditing();
-        editField.onblur = function(ev) {
-          stopEditing();
-          rename();
-        }
-	    };
-    });
-  }
-
   function replaceListInDocument() {
     var target = document.querySelector('ul.todo-list');
     target.innerHTML = '';
@@ -76,6 +20,5 @@ function TodoListView(todoList, document) {
 
   this.render = function() {
     replaceListInDocument();
-    attachListenersForEditing();
   }
 }
