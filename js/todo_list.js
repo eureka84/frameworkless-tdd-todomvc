94839154ead609420cc8163917630ae61b52b198
Matteo Vaccari <vaccari@pobox.com>
2016-11-27 16:29:39 +0100

    move models to their own file

--- js/todo_list.js
+++ js/todo_list.js
@@ -1,46 +1,4 @@
-'use strict';
-
-function TodoItem(text) {
-  var complete = false;
-  var observers = new ObserversList();
-  var parent;
-
-  this.text = function() {
-    return text;
-  }
-
-  this.isCompleted = function() {
-    return complete;
-  }
-
-  this.complete = function(isComplete) {
-    complete = isComplete;
-    observers.notify(this)
-  }
-
-  this.rename = function(newText) {
-    if (text == newText)
-      return;
-    text = newText.trim();
-    observers.notify(this)
-  }
-
-  this.setParent = function(p) {
-    parent = p;
-  }
-
-  this.destroy = function() {
-    parent.destroy(this);
-  }
-
-  this.addObserver = function(observer) {
-    observers.add(observer);
-  }
-
-  this.removeObserver = function(observer) {
-    observers.remove(observer);
-  }
-}
+'use strict'
 
 function TodoList() {
   var todoItems = [];
